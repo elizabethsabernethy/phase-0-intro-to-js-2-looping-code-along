bd92138cbfafd7d2f5589a0b03e02950e58aa965
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-11-23 13:00:40 -0500

    commit tree d5fc130e697babb53b8b3d57c3d76c924b0b07ea

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const messages = [];
+
+function writeCards(name){
+    for(let i=0; i<name.length; i++){
+     messages[i] = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+    } 
+    return messages;
+}
+
+function countDown(number){
+    for(let i=number; i<(number+1) && i>-1; i--){
+        console.log(number);
+        number--;
+    }
+}
